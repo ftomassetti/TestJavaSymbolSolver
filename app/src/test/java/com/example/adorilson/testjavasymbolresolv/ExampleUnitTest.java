af6fe7e83932404bd531cd6037e940aee72b45a2
Federico Tomassetti <federico@tomassetti.me>
2017-11-06 18:55:11 +0100

    adapt the project

--- app/src/test/java/com/example/adorilson/testjavasymbolresolv/ExampleUnitTest.java
+++ app/src/test/java/com/example/adorilson/testjavasymbolresolv/ExampleUnitTest.java
@@ -1,6 +1,17 @@
 package com.example.adorilson.testjavasymbolresolv;
 
+import com.github.javaparser.ParseException;
+import com.github.javaparser.symbolsolver.SourceFileInfoExtractor;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -8,8 +19,21 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+    public TypeSolver typeSolver() throws IOException {
+        return new CombinedTypeSolver(new ReflectionTypeSolver(),
+                new JavaParserTypeSolver(new File("/Users/federico/repos/TestJavaSymbolSolver/app/src/main/java")),
+                new JarTypeSolver("/Users/federico/repos/TestJavaSymbolSolver/libs/android.jar"));
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        //assertEquals(4, 2 + 2);
+    public void runAll() throws IOException, ParseException {
+        File src = new File("/Users/federico/repos/TestJavaSymbolSolver/app/src/main/java");
+        TypeSolver typeSolver = typeSolver();
+        SourceFileInfoExtractor sourceFileInfoExtractor = new SourceFileInfoExtractor();
+        sourceFileInfoExtractor.setTypeSolver(typeSolver);
+        sourceFileInfoExtractor.solve(src);
+        System.out.println("OK " + sourceFileInfoExtractor.getOk());
+        System.out.println("KO " + sourceFileInfoExtractor.getKo());
+        System.out.println("UNSUPPORTED " + sourceFileInfoExtractor.getUnsupported());
     }
 }
